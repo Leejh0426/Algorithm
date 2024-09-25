fd5b02e573e91d088e9e724548991964634bc66e
Leejh0426 <100777451+Leejh0426@users.noreply.github.com>
2024-09-25 14:43:12 +0900

    [Gold II] Title: 컵라면, Time: 52 ms, Memory: 6244 KB -BaekjoonHub

--- 백준/Gold/1781. 컵라면/컵라면.cc
+++ 백준/Gold/1781. 컵라면/컵라면.cc
@@ -1,35 +1,26 @@
-#include<bits/stdc++.h>
-
-using namespace std;
-
-int n;
+#include <bits/stdc++.h> 
+using namespace std; 
+int n, a, b, ret; 
 vector<pair<int, int>> v;
-priority_queue<int,vector<int>, greater<int>> pq;
-
-int ret;
-
+priority_queue<int, vector<int>, greater<int>> pq; 
 int main() {
-	cin >> n;
-
-	int deadline;
-	int ramenCount;
-	for (int i = 0; i < n; i++) {
-		cin >> deadline >> ramenCount;
-		v.push_back({ deadline,ramenCount });
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+	cin >> n; 
+	for(int i = 0; i < n; i++){
+		cin >> a >> b; 
+		v.push_back({a, b}); 
 	}
-	sort(v.begin(), v.end());
-
-	for (pair<int, int>p : v) {
-		pq.push(p.second);
-		if (p.first < pq.size()) {
-			pq.pop();
+	sort(v.begin(), v.end()); 
+	for(int i = 0; i < v.size(); i++){ 
+		ret += v[i].second; 
+		pq.push(v[i].second); 
+		if(pq.size() > v[i].first){
+			ret -= pq.top();  
+			pq.pop(); 
 		}
 	}
-	while (!pq.empty()) {
-		int temp = pq.top();
-		pq.pop();
-		ret += temp;
-	}
-	cout << ret;
-	return 0;
+	cout << ret << "\n";  
+    return 0;
 }
