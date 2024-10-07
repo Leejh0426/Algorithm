232a6f4b9f44f6fcf63fda0a2ac85c092329df30
Leejh0426 <100777451+Leejh0426@users.noreply.github.com>
2024-10-07 10:59:48 +0900

    [Gold III] Title: 소수의 연속합, Time: 32 ms, Memory: 13740 KB -BaekjoonHub

---
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.cc
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+bool che[4000001];
+int n, a[2000001], p, lo, hi, ret, sum;
+
+int main() {
+	cin >> n;
+	
+	for (int i = 2; i <= n; i++) {
+		if (che[i]) continue;
+		for (int j = 2 * i; j <= n; j += i) {
+			che[j] = 1;
+		}
+	}
+
+	for (int i = 2; i <= n; i++) {
+		if (!che[i]) a[p++] = i;
+	}
+
+	while (1) {
+		if (sum >= n) sum -= a[lo++];
+		else if (hi == p) break;
+		else sum += a[hi++];
+		if (sum == n) ret++;
+
+	}
+	cout << ret;
+
+	return 0;
+	
+}
+
+
+
+
+
