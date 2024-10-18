f06a6f0968e99f8ba99b5d7a2517a53d2a6136a2
Leejh0426 <100777451+Leejh0426@users.noreply.github.com>
2024-10-18 13:24:49 +0900

    [Silver IV] Title: 암기왕, Time: 536 ms, Memory: 8296 KB -BaekjoonHub

--- 백준/Silver/2776. 암기왕/암기왕.cc
+++ 백준/Silver/2776. 암기왕/암기왕.cc
@@ -6,23 +6,46 @@ int t, n, m;
 
 
 int temp;
+vector<int> v1;
+
+
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
     cin >> t;
     while (t--) {
-        map<int, bool> _map;
+ 
+        v1.clear();
         cin >> n;
         for (int i = 0; i < n; i++) {
             cin >> temp;
-            _map[temp] = true;
+            v1.push_back(temp);
         }
+        sort(v1.begin(), v1.end());
         cin >> m;
+
         for (int i = 0; i < m; i++) {
             cin >> temp;
-            if (_map[temp]) cout << "1" << '\n';
-            else cout << "0" << "\n";
+            int left = 0, right = v1.size() - 1;
+            int mid;
+            bool flag = false;
+            while (left <= right) {
+                mid = (left + right) / 2;
+                if (v1[mid] == temp) {
+                    cout << "1" << '\n';
+                    flag = true;
+                    break;
+                }
+                else if (v1[mid] > temp) {
+                    right = mid - 1;
+                }
+                else {
+                    left = mid + 1;
+                }
+            }
+            if (!flag) cout << "0" << "\n";
+            
         }
 
     }
