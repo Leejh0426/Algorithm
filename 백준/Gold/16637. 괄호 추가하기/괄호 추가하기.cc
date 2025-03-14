f223240ab21f8ccb7ba134c75ff8f7e435119bca
Leejh0426 <100777451+Leejh0426@users.noreply.github.com>
2025-03-14 10:26:27 +0900

    [Gold III] Title: 괄호 추가하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/16637. 괄호 추가하기/괄호 추가하기.cc
+++ 백준/Gold/16637. 괄호 추가하기/괄호 추가하기.cc
@@ -1,43 +1,89 @@
 #include <bits/stdc++.h>
-using namespace std;  
 
-vector<int> num; 
-vector<char> oper_str; 
-int n, ret = -987654321;
-string s; 
-void fastIO(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL); 
-    cout.tie(NULL);   
-} 
-int oper(char a, int b, int c){
-    if(a == '+') return b + c; 
-    if(a == '-') return b - c; 
-    if(a == '*') return b * c;  
-} 
+using namespace std;
 
-void go(int here, int _num){
-    if(here == num.size() - 1){ 
-        ret = max(ret, _num); 
-        return;
-    }  
-    go(here + 1, oper(oper_str[here], _num, num[here + 1]));
+int dy[] = { -1,0,1,0 };
+int dx[] = { 0,1,0,-1 };
 
-    if(here + 2 <= num.size() - 1){
-        int temp = oper(oper_str[here + 1], num[here + 1], num[here + 2]); 
-        go(here + 2, oper(oper_str[here], _num, temp));  
-    } 
-    return;
-} 
-int main(){
-    fastIO();
-    cin >> n;  
-    cin >> s; 
-    for(int i = 0; i < n; i++){
-        if(i % 2 == 0)num.push_back(s[i] - '0');
-        else oper_str.push_back(s[i]);
-    } 
-    go(0, num[0]);  
-    cout << ret << "\n";
-    return 0;
-} 
+int n;
+
+char c[20];
+int num[20];
+
+int check1 = 0;
+int check2 = 0;
+
+int ret = -999999;
+
+int math(char c, int a, int b) {
+	if (c == '+') {
+		return a + b;
+	}
+	else if (c == '*') {
+		return a * b;
+	}
+	else {
+		return a - b;
+	}
+}
+
+void go(int start, int sum) {
+	int temp = sum;
+
+	if (start > check2) {
+		ret = max(ret, temp);
+		return;
+	}
+
+	if (start + 2 <= check1) {
+		int midTemp = math(c[start + 1], num[start + 1], num[start + 2]);
+		go(start + 2, math(c[start], temp, midTemp));
+	}
+	go(start + 1, math(c[start], temp, num[start + 1]));
+}
+
+int main() {
+
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		if (i % 2 == 0) {
+			cin >> num[check1++];
+		}
+		else {
+			cin >> c[check2++];
+		}
+
+	}
+
+	go(0, num[0]);
+
+	cout << ret;
+
+	return 0;
+}
+//for (int i = 0; i <= check2; i++) {
+//	if (c[i] == '*') {
+//		if (c[i + 1] != '+') {
+//			ret *= num[i + 1];
+//		}
+//		else {
+//			ret *= (num[i + 1] + num[i + 2]);
+//			i++;
+//		}
+//	}
+//	else if (c[i] == '+') {
+//		ret += num[i + 1];
+//	}
+//	else {
+//		if (c[i + 1] != '-') {
+//			ret -= num[i + 1];
+//		}
+//		else {
+//			ret -= (num[i + 1] - num[i + 2]);
+//			i++;
+//		}
+//	}
+//
+//	cout << ret << ' ';
+//}
