0d721561740824ca92afc81d0b134cacd3df7654
Leejh0426 <100777451+Leejh0426@users.noreply.github.com>
2025-03-15 15:07:22 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 12 ms, Memory: 3200 KB -BaekjoonHub

--- 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.cc
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.cc
@@ -1,57 +1,70 @@
 #include <bits/stdc++.h>
+
 using namespace std;
-#define prev aaa
 
-const int max_int = 200000;
+int n, k;
 
-int visited[max_int + 4];
+const int max_n = 100004;
 
-int prev[max_int+4];
+int visited[max_n];
+int ret;
 
-queue<int> q;
-vector<int> v;
 
-int n, k,ret;
+void bfs() {
+	queue<int> q;
+	q.push(n);
+	while (true) {
+		ret++;
+		int qSize = q.size();
+		for (int i = 0; i < qSize; i++) {
 
+			int here = q.front();
+			q.pop();
+
+			if (here == k) return;
+
+			if (here+1 <max_n && visited[here + 1] == -2) {
+				q.push(here + 1);
+				visited[here + 1] = here;
+			}
+			if (here *2 < max_n && visited[here * 2] == -2) {
+				q.push(here * 2);
+				visited[here * 2] = here;
+			}
+			if (here - 1 >= 0 && here-1 < max_n && visited[here - 1] == -2) {
+				q.push(here - 1);
+				visited[here - 1] = here;
+			}
+		}
+	}
+}
 
 
 int main() {
 	cin >> n >> k;
-	if (n == k) {
-		cout << "0" << "\n";
-		cout << n;
-		return 0;
-	}
-	visited[n] = 1;
-	q.push(n);
 
-	while (!q.empty()) {
-		int num = q.front();
-		q.pop();
-		if (num == k) {
-			ret = visited[k];
+	fill(&visited[0], &visited[0]+ max_n, -2);
+
+	visited[n] = -1;
+	bfs();
+	cout << ret - 1 << '\n';
+
+	vector<int> v;
+	v.push_back(k);
+	int temp = k;
+
+	while (true) {
+		if (visited[temp] != -1) {
+			temp = visited[temp];
+			v.push_back(temp);
+		}
+		else {
 			break;
 		}
-		for (int next : {num - 1, num + 1, num * 2}) {
-			if (next >= 0 && next < 200000) {
-				if (!visited[next]) {
-					q.push(next);
-					visited[next] = visited[num] + 1;
-					prev[next] = num;
-				}
-				
-			}
-		}
-
 	}
-	
-	for (int i = k; i != n; i = prev[i]) {
-		v.push_back(i);
-	}
-	v.push_back(n);
 
-	cout << ret - 1 << "\n";
 	reverse(v.begin(), v.end());
+
 	for (int i : v) cout << i << ' ';
 
 	return 0;
