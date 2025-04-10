d8f935d3ebc0641f51e09c78ffc81dcda89e1e66
Leejh0426 <100777451+Leejh0426@users.noreply.github.com>
2025-04-10 16:12:45 +0900

    [level 2] Title: 피보나치 수, Time: 3.54 ms, Memory: 6.93 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.cpp
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.cpp
@@ -9,10 +9,10 @@ int fib(int n){
     if(n==0) return 0;
     if(n==1) return 1;
     if(arr[n]) return arr[n];
-    arr[n] = fib(n-1)%1234567 + fib(n-2)%1234567;
+    arr[n] = (fib(n-1)%1234567 + fib(n-2)%1234567)%1234567;
     return arr[n];
 }
 
 int solution(int n) {
-    return fib(n)%1234567;
+    return fib(n);
 }
