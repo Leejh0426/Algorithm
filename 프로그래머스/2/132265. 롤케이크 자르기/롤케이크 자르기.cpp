89b0124fae65f6ed5e208d7314be0327532ed8a0
Leejh0426 <100777451+Leejh0426@users.noreply.github.com>
2025-05-05 16:39:10 +0900

    [level 2] Title: 롤케이크 자르기, Time: 78.99 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.cpp
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.cpp
@@ -4,50 +4,32 @@
 
 using namespace std;
 
-bool arr[10004];
-bool reversearr[10004];
 
-int sortt[1000003];
-int reversesortt[1000004];
-
-
-void go(vector<int> &topping){
-    int sort1=0;
-    int reversesort1=0;
-    for(int i=0;i<topping.size();i++){
-        if(!arr[topping[i]]){
-            sortt[i]=++sort1;
-            arr[topping[i]]=true;
-        }
-        else{
-            sortt[i]=sort1;
-        }
-        //cout<<"sortt[i]: "<<sortt[i]<<'\n';
-    }
-    
-    for(int i=topping.size()-1;i>=0;i--){
-        if(!reversearr[topping[i]]){
-            reversesortt[i]=++reversesort1;
-            reversearr[topping[i]]=true;
-        }
-        else{
-            reversesortt[i]=reversesort1;
-        }
-    } 
-    return;
-}
 
 
 int solution(vector<int> topping) {
-    int answer = 0;
+    int answer=0;
     
-    go(topping);
-
-    for(int i=0; i<topping.size()-1;i++){
-       // cout<<sortt[i]<<" "<<reversesortt[i+1]<<'\n';
-        if(sortt[i]==reversesortt[i+1]) answer++;
-    } 
-
+    unordered_map<int,int> topping_cnt;
+    unordered_set<int> half_topping;
+    
+    for(auto top : topping){
+        if(topping_cnt.find(top)==topping_cnt.end()){
+            topping_cnt[top]=1;
+        }
+        else{
+            topping_cnt[top]++;
+        }
+    }
+    for(int i=0; i<topping.size();i++){
+        half_topping.insert(topping[i]);
+        topping_cnt[topping[i]]--;
+        
+        if(topping_cnt[topping[i]] == 0){
+            topping_cnt.erase(topping[i]);
+        }
+        if(topping_cnt.size()==half_topping.size()) answer++;
+    }
     
     return answer;
 }
