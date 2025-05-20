a219c41450fe4a808af045efbc0d23087a06d35f
Leejh0426 <100777451+Leejh0426@users.noreply.github.com>
2025-05-20 17:51:08 +0900

    [level 2] Title: 땅따먹기, Time: 7.27 ms, Memory: 27.3 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.cpp
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.cpp
@@ -9,16 +9,29 @@ int answer;
 
 int solution(vector<vector<int> > land)
 {
-    for(int i=1; i<=land.size(); i++){
-        for(int j=1; j<=land[i-1].size();j++){
-            int temp=0;
-            for(int k=0; k<4; k++){
-                if(k!=j-1) temp = max(arr[i-2][k],temp);
-            }
-            arr[i-1][j-1] = temp+land[i-1][j-1];
-            answer=max(answer,arr[i-1][j-1]);
-        }    
-    }
+    int n=land.size();
     
+    arr[0][0] = land[0][0];
+    arr[0][1] = land[0][1];
+    arr[0][2] = land[0][2];
+    arr[0][3] = land[0][3];
+    
+    
+    for(int i=1; i<n; i++)
+    {
+        for(int j=0;j<4;j++)
+        {
+            int max_num=0;
+            for(int k=0; k<4; k++)
+            {
+                if(k==j) continue;
+                max_num = max(max_num, arr[i-1][k]);
+            }
+            arr[i][j] = max_num + land[i][j];
+        }
+    }
+    for(int i=0; i<4; i++){
+        answer = max(answer, arr[n-1][i]);
+    }
     return answer;
 }
